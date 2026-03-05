b4f9c668e38567dcd900bb9476aa48ad38868ddb
nurulamin092 <mnurulamin092@gmail.com>
2026-03-05 14:16:08 +0600

    demo push

--- src/app.ts
+++ src/app.ts
@@ -32,7 +32,9 @@ app.use(
       }
       return callback(new Error("Not allowed by CORS"));
     },
+
     credentials: true,
+
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: [
       "Content-Type",
